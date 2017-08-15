fa7a3498303a8e52f735fe704f4d4fb23451d6d7
Abhishekmamidi <abhishek.m15@iiits.in>
2017-08-15 12:24:05 +0530

    Add files via upload

--- Assignment-1/search.py
+++ Assignment-1/search.py
@@ -2,26 +2,58 @@
 # 1. Given a set of files with data in it.
 # 2. A Query is given and the respective file_names should be displayed.
 
+# Process:
+# Find PathOfFiles.
+# Extract data from xml files.
+# Apply tokenization on the data.
+# Remove stopwords(english) from the data.
+# Apply Stemming on every word.
+# Create an index table of words with frequency and the file numbers to which the word belong.
+# Sort the words and merge the repeating words.
+# Based on the query given, print the respective filenames.
+
+
 import os
 import sys
 from nltk.stem import PorterStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize, wordpunct_tokenize
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
+import cPickle as pickle
+import json
+# Make it work for Python 2+3 and with Unicode
+import io
+try:
+    to_unicode = unicode
+except NameError:
+    to_unicode = str
 
 
-listOfFilepaths=[]
+stemmedWords = []
+# The main dictionary in which all the data is stored.
+DictionaryOfWords = {}
+stop_words = set(stopwords.words('english'))
+stop_words.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}']) # remove it if you need punctuation
+porter = PorterStemmer()
+
+
+# Get the paths of all files in the directory and store it in the list  "listOfFilepaths"
+listOfFilepaths={}
 def get_files_in_directory(path):
-	# count = 0
-	# DictionaryOfWords[filepaths]={}
+	count = 0
 	for root, dirs, files in os.walk(path):
         	if len(files)!=0:
 				for file in files:
-					listOfFilepaths.append(root+"/"+file)
+					listOfFilepaths[count]=(root+"/"+file)
 					count+=1
+	DictionaryOfWords["filenames"]=listOfFilepaths
+	# print DictionaryOfWords
+	for word in DictionaryOfWords:
+		print word, DictionaryOfWords[word]
+	print "\n"
+	print listOfFilepaths
 	# for file_path in listOfFilepaths:
 	# 	print file_path
-
 
 # Extract contents and process them.
 def extractData(filename):
@@ -31,12 +63,8 @@ def extractData(filename):
 		data = "".join(line.rstrip() for line in file)
 		# lower case the words
 		data = data.lower()
-		# print data
-		# print "abhi\n"
 		# Use BeautifulSoup to read xml format
 		soup = BeautifulSoup(data, 'html.parser')
-		# print soup
-		# print "shek\n"
 		# Exraxt the text from the respective tags
 		title = soup.find("title").get_text()
 		text = soup.find("text").get_text()
@@ -48,39 +76,56 @@ def extractData(filename):
 		for word in listOfTextContent:
 			if word not in stop_words:
 				stemmedWords.append(porter.stem(word))
-		# print stemmedWords
-		# print "\n\n\n"
 		# stemmedWords contains all the words(tokenized and stemmed) except stopwords
 
-
-def pushToDict(filename):
-	# {{word: {filename: frequency, filename2: frequency}}}
+def pushToDict(fileno):
+	# {{word: {fileno: frequency, fileno: frequency}}}
 	for word in stemmedWords:
 		if word not in DictionaryOfWords:
 			DictionaryOfWords[word] = {}
-			DictionaryOfWords[word][filename] = 1
+			DictionaryOfWords[word][fileno] = 1
 		else:
 			temp = DictionaryOfWords[word]
-			if filename in temp:
-				DictionaryOfWords[word][filename]+=1
+			if fileno in temp:
+				DictionaryOfWords[word][fileno]+=1
 			else:
-				DictionaryOfWords[word][filename] = 1
+				DictionaryOfWords[word][fileno] = 1
 
 
-
+# main function
 # argument(folder path) is given in the command line
 path=sys.argv[1]
 # Call get_files_in_directory function to get the list of all the paths of the files
 get_files_in_directory(path)
-print ""
 # Iterate through every file and extractData
-count=0
-for filename in listOfFilepaths:
-	# filename=listOfFilepaths[filePath]
-	# print filename
-	extractData(filename)
-	pushToDict(count)
-	count+=1
-	print count
-	
+for fileno in listOfFilepaths:
+	extractData(listOfFilepaths[fileno])
+	pushToDict(fileno)
+	print fileno
+
+# Just for printing
+# for word in DictionaryOfWords:
+# 	print word, DictionaryOfWords[word]
+# print "\n"
+
+# Store in a json file
+with io.open('data.json', 'w', encoding='utf8') as outfile:
+    str_ = json.dumps(DictionaryOfWords,
+                      indent=4, sort_keys=True,
+                      separators=(',', ': '), ensure_ascii=False)
+    outfile.write(to_unicode(str_))
+
+# Open and read an json file
+# with open('data.json') as data_file:
+#     data_loaded = json.load(data_file)
+# for word in data_loaded:
+# 	print word, data_loaded[word]
+
+# Query search
+# query = "total newspaper"
+# queryList = wordpunct_tokenize(query)
+#
+# for word in queryList:
+# 	if word not in stop_words:
+# 		print word, DictionaryOfWords[porter.stem(word)]
 
